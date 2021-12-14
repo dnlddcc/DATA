5cbab2df4e4f7153a88ce2b32155290c072d14d5
dnlddcc <95754514+dnlddcc@users.noreply.github.com>
2021-12-14 17:29:06 +0100

    Batch update

---
+++ js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-files","displayName":"Files","link":{"linkType":"LinkTypePage","href":"#!page-files"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
