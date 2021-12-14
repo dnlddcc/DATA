24cfcdc89921b9d597ee41c596d21200f36d151c
dnlddcc <95754514+dnlddcc@users.noreply.github.com>
2021-12-14 17:43:59 +0100

    Batch update

---
+++ js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-summary","displayName":"SUMMARY","link":{"linkType":"LinkTypePage","href":"#!page-summary"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
