84b7364d1fadb5b2c5961a87ec232f73f1e6251b
dnlddcc <95754514+dnlddcc@users.noreply.github.com>
2021-12-14 18:02:10 +0100

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-summary","displayName":"SUMMARY","link":{"linkType":"LinkTypePage","href":"#!page-summary"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-page-1","displayName":"Page 1","link":{"linkType":"LinkTypePage","href":"#!page-page-1"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
